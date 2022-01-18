70157a3581f16819958344d2c36599109e5e9c7e
awojty <awojtyna@mit.edu>
2022-01-17 21:30:04 -0500

    refactor activeplayers

--- client/src/components/functional/ActivePlayers.js
+++ client/src/components/functional/ActivePlayers.js
@@ -1,19 +1,32 @@
 import React from 'react'
-import Score from './Score'
-
-export default function ActivePlayers({data}) {
-    return (
-        <div className='score-board'>
-             <h1 className="score-title">Active Players</h1>
-            {this.props.players.map(element=>
-            <div>{element}</div>
+import React, { Component } from 'react'
 
 
+class ActivePlayers extends Component {
+    constructor(props){
+        super(props)
+    }    
 
 
-            )}
-           
-           
-        </div>
-    )
+    render(){
+        return (
+            <div className='score-board'>
+                 <h1 className="score-title">Active Players</h1>
+                {this.props.players.map(element=>
+                <div>{element}</div>
+    
+    
+    
+    
+                )}
+               
+               
+            </div>
+        )
+
+
+    }
+    
 }
+
+export default ActivePlayers
