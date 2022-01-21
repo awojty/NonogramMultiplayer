64891d5322f11277919a66be82ceb02d2ba24f1f
awojty <awojtyna@mit.edu>
2022-01-21 18:38:30 -0500

    remove 4000 port

--- client/src/components/pages/Invitation.js
+++ client/src/components/pages/Invitation.js
@@ -17,8 +17,8 @@ import { useParams } from "react-router-dom";
 
 import socketIOClient from 'socket.io-client'
 //TODO - choises a game froma  alink 
-const ENDPOINT = 'http://localhost:4000/'
-//const ENDPOINT = 'http://multi-nonogram.herokuapp.com/'
+//const ENDPOINT = 'http://localhost:4000/'
+const ENDPOINT = 'http://multi-nonogram.herokuapp.com/'
 
 class Invitation extends React.Component {
     constructor(props){
